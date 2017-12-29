9c22bae085ec796e996d4e2bcb05550177477e4c
林永璋 <yclin@jumbogames.com.tw>
2017-12-29 11:51:07 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+var world_width = 640;
+var world_height = 640;
+
+var game = new Phaser.Game(world_width, world_height, Phaser.AUTO, null, {
+    preload: preload, create: create, update: update
+});
+
+var ball;
+
+function preload() {
+
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.stage.backgroundColor = '#eeeeee';
+
+    game.load.image('ball', 'assets/img/ball.jpg');
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.x = 1000;
+    game.physics.arcade.gravity.y = 1000;
+}
+
+function create() {
+
+    ball = game.add.sprite(20, 20, 'ball');
+    ball.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    game.physics.enable(ball); //必須要先設定才能有以下屬性
+    
+    ball.enableBody = true;
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.setTo(1);
+
+}
+
+function update() {
+
+}
+
+function handleRemoteImagesOnJSFiddle() {
+    
+}
